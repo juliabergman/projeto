eedae256385db24304d5b85763bc6444820b41e7
JuliaBergman <juliabergman523@gmail.com>
2020-06-09 18:35:53 -0300

    fotos páginas quemsomos

--- src/components/testimony/Testimony.js
+++ src/components/testimony/Testimony.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-
 import './testimony.css';
-
-import Taylor from '../../assets/images/taylor.jpg'
 import Julia from '../../assets/images/quemsomos/juliagostosa.jpg'
 import Ana from '../../assets/images/quemsomos/anagostosa.jpeg'
 import Lucas from '../../assets/images/quemsomos/lucasgostoso.jpeg'
 import Filipe from '../../assets/images/quemsomos/filipegostoso.jpeg'
+import Matheus from '../../assets/images/quemsomos/matheusgostoso.jpeg'
+import LucasLolo from '../../assets/images/quemsomos/lologostoso.jpeg'
+// import Instagram from '../../assets/images/quemsomos/instagram.png'
+// import Twitter from '../../assets/images/quemsomos/twitter.png'
+// import Youtube from '../../assets/images/quemsomos/youtube.png'
+// import Github from '../../assets/images/quemsomos/github.png'
 export default class Testimony extends Component {
   render() {
     return (
@@ -21,6 +24,15 @@ export default class Testimony extends Component {
           <div className="text-box">
             <p>"Atualmente sou a "maluca dos projetos", e as vezes não tenho muito tempo pra presentear as pessoas que amo, mas claro que não deixaria passar a data em branco. Tive essa ideia duas semanas antes da data, então eu diria que foi quase um projeto suicida, porém, já passei do tempo de morar na zona de conforto. E claramente eu não seria nada sem a minha equipe (ou startap, talvez) que topou essa loucura comigo. Não sou nada sem vocês!"</p>
           </div>
+
+
+          
+          <div className="redessociais">
+
+  
+
+          </div>
+
         </div>
 
         <div className="tstm">
@@ -84,7 +96,7 @@ export default class Testimony extends Component {
 
         <div className="tstm">
           <div className="tst-info">
-            <img src={Taylor} alt="Imagem do colaborador" />
+            <img src={LucasLolo} alt="Imagem do colaborador" />
             <h3>Lucas Lorenzetti</h3>
             <p>Editor de fotos</p>
           </div>
@@ -102,7 +114,7 @@ export default class Testimony extends Component {
 
         <div className="tstm">
           <div className="tst-info">
-            <img src={Taylor} alt="Imagem do colaborador" />
+            <img src={Matheus} alt="Imagem do colaborador" />
             <h3>Matheus Souza Silva</h3>
             <p>desenvolvedor</p>
           </div>
