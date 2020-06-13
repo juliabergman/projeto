b848de7afbaf351176ecb350ecdaa9e14bb46659
DiogoBotton <diogo.luo@gmail.com>
2020-06-13 06:53:23 -0300

    atualizacao depoimentos, disfarcada no login, desculpa :'(

--- src/components/depoimento/Depoimento.js
+++ src/components/depoimento/Depoimento.js
@@ -3,12 +3,16 @@ import React, { Component } from 'react';
 import './depoimento.css';
 
 import User from '../../assets/images/userfoto.png.png'
+import img1 from '../../assets/images/funkeiro.jpg';
+import img2 from '../../assets/images/stalin.jpg';
+import img3 from '../../assets/images/bozo.jpg';
 
 export default class depoimento extends Component {
   render() {
     return (
       <div>
-        <div className="depoimentotstm">
+
+        {/* <div className="depoimentotstm">
           <div className="depoimentotst-info">
             <img src={User} alt="Imagem do usuario" />
             <h3>{this.props.name}</h3>
@@ -18,7 +22,60 @@ export default class depoimento extends Component {
           <div className="text-box3">
             <p>{this.props.name}</p>
           </div>
+        </div> */}
+
+        <div className="depoimentotstm">
+          <div>
+
+          <div className="depoimentotst-info">
+            <img src={img1} alt="Imagem do usuario" />
+            <h3>Vitinhooo</h3>
+            <p>Visitante</p>
+          </div>
+
+          <div className="text-box3">
+            <p>vose È muito gata, gata.</p>
+          </div>
+
+          <div className="depoimentotst-info">
+            <img src={User} alt="Imagem do usuario" />
+            <h3>Anonymous</h3>
+            <p>Visitante</p>
+          </div>
+
+          <div className="text-box3">
+            <p>vc lembrah de mim anah vc tinha invejah de mim sua recalkadah</p>
+          </div>
+          
+          </div>
+
+          <div>
+
+          <div className="depoimentotst-info">
+            <img src={img2} alt="Imagem do usuario" />
+            <h3>Josef Stalin</h3>
+            <p>Visitante</p>
+          </div>
+
+          <div className="text-box3">
+            <p>Salve a União Soviética, minha camarada aninhahhh! :p</p>
+          </div>
+
+          <div className="depoimentotst-info">
+            <img src={img3} alt="Imagem do usuario" />
+            <h3>Bozonaro</h3>
+            <p>Lixo</p>
+          </div>
+
+          <div className="text-box3">
+            <p>uNiAo SoviEtcA È mEu brIoCo, tAL Key?!</p>
+          </div>
+
+          </div>
         </div>
+
+        
+        
       </div>
     );
   }
--- src/paginas/login/Login.js
+++ src/paginas/login/Login.js
@@ -1,23 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './login.css';
 import {Link} from 'react-router-dom';
 
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
 
-function Login(){
+        this.state = {
+            email: "",
+            senha: "",
+            erroMensagem : "",
+            isLoading : false
+        }
+    }
+
+    // Atualiza estado genérico, para que seja feito uma só vez.
+    atualizaEstado = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    realizarLogin(event){
+        event.preventDefault();
+
+        // Limpa o conteúdo do state erroMensagem
+        this.setState({ erroMensagem : '' })
+
+        // Define que uma requisição está em andamento
+        this.setState({ isLoading : true })
+
+        let usuario = {
+            email: this.state.email,
+            senha: this.state.senha
+        }
+        
+        if(usuario.email === 'aninhahgatinhah@youtlook.com' && usuario.senha === 'odeiorecalcadas123'){
+            this.props.history.push('/home');
+        }
+        else{
+            this.setState({ erroMensagem : 'piiii piii pii OLHA O RECALQUEEEE vc nao eh a ana e eh so uma recakkadah!' })
+            this.setState({ isLoading : false })
+        }
+
+    }
+
+render(){
     return(
         <div>
            
-        <div id="teste">         
+        <div id="teste">
+        <form onSubmit={this.realizarLogin.bind(this)}>         
         <div className="login">
             <label>Email</label>
-            <input placeholder="Digite seu email aqui..."/>
+            <input 
+                placeholder="Digite seu email aqui..."
+                className="input__login"
+                type="text"
+                name="email" // Deve ser igual ao nome da variável no state para que o atualizaEstado funcione.
+                value={this.state.email}
+                onChange={this.atualizaEstado}
+                id="login__email"
+            />
             <label >Senha</label>
-            <input placeholder="Digite sua senha aqui..."/>           
-            <Link className="linklogin" to='/home' title="Home da ana">Entrar</Link>
+            <input 
+                placeholder="Digite sua senha aqui..."
+                className="input__login"
+                type="password"
+                name="senha"
+                value={this.state.senha}
+                onChange={this.atualizaEstado}
+                id="login__password"
+            />           
+            {/* <Link className="linklogin" to='/home' title="Home da ana">Entrar</Link> */}
             <Link className="linklogin" to='/homevisitante' title="Home do visitante do site">Visitante</Link>
-           
+            <p style={{ color : 'red' }}>{this.state.erroMensagem}</p>
+            {
+                this.state.isLoading === true &&
+                <div className="item">
+                    <button type="submit" className="btn btn__login" id="btn__login" disabled>Loading...</button>
+                </div>
+            }
+            {
+                this.state.isLoading === false &&
+                <div className="item"> 
+                    <button type="submit" className="btn btn__login" id="btn__login">Login</button>
+                </div>
+            }
         </div>
-       
+       </form>
         </div>
        
         </div>
@@ -27,5 +96,5 @@ function Login(){
     
 
 }
-
-export default Login;
+}
+// export default Login;
